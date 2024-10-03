df3efa40269df923d0687cadbc45da5253edcd0f
aspauldingcode <aspauldingcode@gmail.com>
2024-10-02 21:52:05 -0600

    add OutliningController thingy

---
+++ StopStoplightLight/WindowOutliningController.h
@@ -0,0 +1,30 @@
+//
+//  WindowOutliningController.h
+//  StopStoplightLight
+//
+//  Created by Alex Spaulding on 10/2/24.
+//  Copyright © 2024 Shishkabibal. All rights reserved.
+//
+//  Created by j on 1/19/20.
+//  Copyright © 2020 j. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WindowOutliningController : NSObject
+@property (strong) NSMenuItem *accentColorItem;
+@property (strong) NSMenuItem *whiteItem;
+@property (strong) NSMenuItem *blackItem;
+@property (strong) NSMenuItem *redItem;
+@property (strong) NSMenuItem *greenItem;
+@property (strong) NSMenuItem *blueItem;
+@property (strong) NSMenuItem *yellowItem;
+@property (strong) NSMenuItem *orangeItem;
+@property (strong) NSMenuItem *purpleItem;
+@property (strong) NSArray *colorItems;
+@property (strong) NSMenu *menu;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StopStoplightLight/WindowOutliningController.m
@@ -0,0 +1,8 @@
+//
+//  WindowOutliningController.m
+//  StopStoplightLight
+//
+//  Created by Alex Spaulding on 10/2/24.
+//  Copyright © 2024 Shishkabibal. All rights reserved.
+//
+
